8a97212978a5f7574b3bd8036317195706235e07
Anang Nugraha <anangnugraha8@gmail.com>
2019-12-20 18:49:53 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,59 @@
+@extends('master/logintemplate')
+
+@section('title','Login')
+
+@section('app')
+<div id="app">
+    <section class="section">
+      <div class="container mt-2">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+              <img src="{{ asset('img/Pertamina.png') }}" alt="logo" width="275" class="">
+            </div>
+            <div class="card card-primary">
+              <div class="card-header"><h4>Sign In</h4></div>
+              <div class="card-body">
+                <form method="POST" action="#" class="needs-validation" novalidate="">
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <div class="invalid-feedback">
+                      Please fill in your email
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="d-block">
+                    	<label for="password" class="control-label">Password</label>
+                      <div class="float-right">
+                        
+                      </div>
+                    </div>
+                    <input id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <div class="invalid-feedback">
+                      please fill in your password
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                      Login
+                    </button>
+                  </div>
+                </form>
+                
+
+              </div>
+            </div>
+            <div class="mt-2 text-muted text-center">
+              Don't have an account? <a href="{{url('/register')}}">Create One</a>
+            </div>
+            <div class="simple-footer">
+              Copyright &copy; Stisla 2018
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+  @endsection
